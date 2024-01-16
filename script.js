30d3cf7d318df787b7c268572853d90a191b137d
GiuliaCannella <giucanni99@gmail.com>
2024-01-16 17:43:37 +0100

    add js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // console.log("js ok");
+
+/*
+ Chiedi all'utente il suo nome
+ Chiedi poi il suo cognome
+ Chiedi il colore preferito
+ Infine stampa la stringa in questo modo:
+  nomecognomecolorepreferito21!
+*/
+
+const nomeUser= prompt("scrivi il tuo nome","Giulia");
+const cognomeUser= prompt("scrivi il tuo cognome", "Cannella")
+const coloreUser= prompt("scrivi il tuo colore preferito", "rosso")
+const dati=nomeUser+cognomeUser+coloreUser+"21!"
+
+console.log(typeof(nomeUser), 'Nome user');
+console.log(typeof(cognomeUser), 'Cognome user');
+console.log(typeof(coloreUser), 'Colore');
+
+console.log(dati);
